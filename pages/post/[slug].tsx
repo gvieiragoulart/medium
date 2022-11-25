9f451adc38825b5f10dd66a83e3215e32b6261d1
Gabriel Vieira <gabriel.goulart@tradetechnology.com.br>
2022-11-25 17:38:56 -0300

    post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,8 @@
 import { GetStaticProps } from "next"
 import Header from "../../components/Header"
-import { sanityClient } from "../../sanity"
+import { sanityClient, urlFor } from "../../sanity"
 import { Post } from '../../typings'
+import PortableText from "react-portable-text"
 
 
 interface Props {
@@ -9,10 +10,31 @@ interface Props {
 }
 
 function Post({post}: Props) {
-    console.log(post);
   return (
     <main>
         <Header/>
+
+        <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url()!} alt="" />
+
+        <article className="max-w-3xl mx-auto p-5">
+            <h1 className="text-3xl mt-10 mb-3" >{post.title}</h1>
+            <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+        
+            <div className="flex items-center space-x-2">
+                <img className="h-10 w-10 rounded-full " src={urlFor(post.author.image).url()!} alt="" />
+                <p className="font-extralight text-sm">Blog post by <span className="text-green-600">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
+            </div>
+
+            <div className="mt-10">
+                <PortableText 
+                className=""
+                dataset="production" 
+                projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                content={post.body}
+                />
+            </div>
+        </article>
+    
     </main>
   )
 }
