e0f02117161727ef9ad9f1400d52acc67d1da2fe
Gabriel Vieira <gabriel.goulart@tradetechnology.com.br>
2022-11-30 11:09:59 -0300

    add comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,13 +3,41 @@ import Header from "../../components/Header"
 import { sanityClient, urlFor } from "../../sanity"
 import { Post } from '../../typings'
 import PortableText from "react-portable-text"
+import {useForm, SubmitHandler} from "react-hook-form"
+import { useState } from "react"
 
-
+interface IFormInput {
+    _id: string;
+    name: string;
+    email: string;
+    comment: string;
+}
 interface Props {
     post: Post;
 }
 
 function Post({post}: Props) {
+    const [submitted, setSubmitted] = useState(false);
+
+    const { 
+        register, 
+        handleSubmit, 
+        formState: {errors}, 
+    } = useForm<IFormInput>();
+
+    const onSubmit:SubmitHandler<IFormInput> = async(data) => {
+        await fetch('/api/createComment', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log(data);
+            setSubmitted(true);
+        }).catch((err) => {
+            console.log(err);
+            setSubmitted(false);
+        })
+    };
+
   return (
     <main>
         <Header/>
@@ -31,10 +59,104 @@ function Post({post}: Props) {
                 dataset="production" 
                 projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
                 content={post.body}
+                serializers={{
+                        h1: (props: any) => {
+                            <h1 className="text-2xl font-bold my-5" {...props}/>
+                        },
+                        h2: (props: any) => {
+                            <h1 className="text-xl font-bold my-5" {...props} />
+                        },
+                        li: (children: any) => {
+                            <li className="ml-4 list-disc">{children}</li>
+                        },
+                        link: ({href, children}: any) => {
+                            <a href={href} className="text-blue-500 bover:underline">{children}</a>
+                        }}
+                }
                 />
             </div>
         </article>
-    
+                
+        <hr className="max-w-lg my-5 mx-auto border border-yellow-500"/>
+
+        {submitted ? (
+            <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+                <h3 className="text-3xl font-bold">Thank you for submitting your comment!</h3>
+                <p>
+                    Once it has been approved, it will appear below!
+                </p>
+            </div>
+        ): ( 
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+            <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+            <h4 className="text-2xl font-bold">Leave a comment below!</h4>
+            <hr className="py-3 mt-2"/>
+
+            <input
+                {...register("_id")}
+                name="_id"
+                value={post._id} 
+                type="hidden" 
+                />
+
+            <label className="block mb-5">
+                <span className="text-gray-700">Name</span>
+                <input 
+                    {...register("name", { required: true})}
+                    className="shadow border rounded py-2 px-2 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+                    placeholder="John Appleseed" 
+                    type="text" 
+                    />
+            </label>
+            <label className="block mb-5">
+                <span className="text-gray-700">Email</span>
+                <input
+                {...register("email", { required: true})} 
+                className="shadow border rounded py-2 px-2 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+                placeholder="John Appleseed" 
+                type="email" 
+                />
+            </label>
+            <label className="block mb-5">
+                <span className="text-gray-700">Comment</span>
+                <textarea 
+                    {...register("comment", { required: true})} 
+                    className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+                    placeholder="John Appleseed" 
+                    rows={8}
+                />
+            </label>
+
+            {/* erros will return when field validation fails */}
+            <div className="flex flex-col p-5">
+                {errors.name && (
+                    <span className="text-red-500">- The name field is required</span>
+                )}
+                {errors.comment && (
+                    <span className="text-red-500">- The comment field is required</span>
+                )}
+                {errors.email && (
+                    <span className="text-red-500">- The email field is required</span>
+                )}
+            </div>
+
+            <input type="submit" className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"/>
+        </form>
+        )}
+
+        {/* Comments */}
+        <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+            <h3 className="text-4xl">Comments</h3>
+            <hr className="pb-2" />
+
+            {post.comments.map((comment) => (
+                <div key={comment._id}>
+                    <p>
+                        <span className="text-yellow-500">{comment.name}:</span> {comment.comment}
+                    </p>
+                </div>
+            ))}
+        </div>
     </main>
   )
 }
@@ -72,6 +194,11 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
          name,
          image
         },
+        'comments': *[
+            _type == "comment" &&
+            post._ref == ^._id &&
+            approved == true
+          ],
         description,
         mainImage,
         slug,
